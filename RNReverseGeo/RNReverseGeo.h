342a0e00c536f102987a2b615577e97eed6642d5
ortutay <marcell.ortutay@gmail.com>
2015-07-29 16:02:10 -0700

    Update for React 0.8.0

--- RNReverseGeo/RNReverseGeo.h
+++ RNReverseGeo/RNReverseGeo.h
@@ -5,9 +5,7 @@
 //  Created by Aaron Saunders on 5/11/15.
 //  Copyright (c) 2015 Clearly Innovative Iinc. All rights reserved. -
 //
-#import "RCTBridge.h"
-#import "RCTBridgeModule.h"
-#import "RCTEventDispatcher.h"
+#import "Base/RCTBridgeModule.h"
 
 #import <CoreLocation/CLError.h>
 #import <CoreLocation/CLLocationManager.h>
